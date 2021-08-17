315aa9f200ec37d2679508cd6b8259a15acb724e
김지웅 <godjw96@gmail.com>
2021-08-17 15:35:34 +0900

    Update BubbleSort.java

--- src/chap6/BubbleSort.java
+++ src/chap6/BubbleSort.java
@@ -10,13 +10,39 @@ public class BubbleSort {
         a[idx2] = t;
     }
 
-    // 버블 정렬 벼전1
+//    // 버블 정렬 버전1
+//    static void bubbleSort(int[] a, int n) {
+//        for (int i = 0; i < n - 1; i++) {
+//            for (int j = n - 1; j > i; j--) {
+//                if (a[j - 1] > a[j])
+//                    swap(a, j - 1, j);
+//            }
+//        }
+//    }
+
+//    static void bubbleSort(int[] a, int n) {
+//        for (int i = 0; i < n - 1; i++) {
+//            int exchg = 0;      // 패스의 교환 횟수를 기록합니다.
+//            for (int j = n - 1; j > i; j--) {
+//                if (a[j - 1] > a[j]) {
+//                    swap(a, j - 1, j);
+//                    exchg++;
+//                }
+//                if (exchg == 0) break; // 교환이 이루어지지 않으면 종료합니다.
+//            }
+//        }
+//    }
+
     static void bubbleSort(int[] a, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = n - 1; j > i; j--) {
-                if (a[j - 1] > a[j])
+        int k = 0;              // a[k]보다 앞쪽은 정렬을 마친 상태
+        while (k < n - 1) {
+            int last = n - 1;        // 마지막으로 요소를 교환한 위치
+            for (int j = n - 1; j < k; j--)
+                if (a[j - 1] > a[j]) {
                     swap(a, j - 1, j);
-            }
+                    last = j;
+                }
+            k = last;
         }
     }
 
