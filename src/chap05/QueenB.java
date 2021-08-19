eb363af2df73e2c4ea094a6543115dfc1a449e60
김지웅 <godjw96@gmail.com>
2021-08-20 00:20:51 +0900

    8퀸

---
+++ src/chap05/QueenB.java
@@ -0,0 +1,29 @@
+package chap05;
+
+public class QueenB {
+
+    static int[] pos = new int[8];
+
+    // 각 열의 퀸의 위치를 출력합니다.
+    static void print() {
+        for (int i = 0; i < 8; i++) {
+            System.out.printf("%2d", pos[i]);
+        }
+        System.out.println();
+    }
+
+    // i열에 퀸을 놓습니다.
+    static void set(int i) {
+        for (int j = 0; j < 8; j++) {
+            pos[i] = j;
+            if (i == 7)
+                print();
+            else
+                set(i + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        set(0);
+    }
+}
