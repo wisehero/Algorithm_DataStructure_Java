6fe8e4736bcec7c618a15b987d8ae44ccb0bea9c
김지웅 <godjw96@gmail.com>
2021-08-25 00:54:27 +0900

    Create QuickSort2.java

---
+++ src/chap06/QuickSort2.java
@@ -0,0 +1,66 @@
+package chap06;
+
+import chap04_Stack_Queue.IntStack;
+
+import java.util.Scanner;
+
+public class QuickSort2 {
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void quickSort(int[] a, int left, int right) {
+        IntStack lstack = new IntStack(right - left + 1);
+        IntStack rstack = new IntStack(right - left + 1);
+
+        lstack.push(left);
+        rstack.push(right);
+
+        while (lstack.isEmpty() != true) {
+            int pl = left = lstack.pop();
+            int pr = right = rstack.pop();
+            int x = a[(left + right) / 2]; // 피벗
+
+            do {
+                // 왼쪽부터 스캔 했을 때 피벗보다 작으면
+                while (a[pl] < x) pl++;
+                // 오른쪽부터 스캔 했을 때 피벗 보다 크면
+                while (a[pr] > x) pr--;
+                if (pl <= pr) {
+                    swap(a, pl++, pr--);
+                }
+            } while (pl <= pr);
+
+            if (left < pr) {
+                lstack.push(left);
+                rstack.push(pr);
+            }
+            if (pl < right) {
+                lstack.push(pl);
+                rstack.push(right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("퀵 정렬");
+        System.out.print("요솟수 : ");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        quickSort(x, 0, nx - 1);
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++)
+            System.out.println("x[" + i + "]=" + x[i]);
+    }
+}
