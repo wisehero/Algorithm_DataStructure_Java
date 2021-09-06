6c04d12d9f24b49d1e41c034f0a427d6f5b6e6af
김지웅 <godjw96@gmail.com>
2021-09-06 21:33:05 +0900

    Create HeapSort_17.java

---
+++ src/chap06/practice/HeapSort_17.java
@@ -0,0 +1,13 @@
+package chap06.practice;
+
+public class HeapSort_17 {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+
+
+}
