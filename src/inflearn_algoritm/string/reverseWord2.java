30ad3562390fc061102145b41e9ae891ec48fc90
김지웅 <godjw96@gmail.com>
2021-10-28 16:44:18 +0900

    Create reverseWord2.java

---
+++ src/inflearn_algoritm/string/reverseWord2.java
@@ -0,0 +1,18 @@
+package inflearn_algoritm.string;
+
+import java.util.Scanner;
+
+public class reverseWord2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isAlphabetic(arr[i])){
+                
+            }
+        }
+    }
+}
