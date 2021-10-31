ad1c3f28a9746db418c9f18e514adde30963e203
김지웅 <godjw96@gmail.com>
2021-10-31 22:21:34 +0900

    Update LinkedList.java

--- src/chap09/LinkedList.java
+++ src/chap09/LinkedList.java
@@ -36,4 +36,52 @@ public class LinkedList<E> {
         }
         return null;
     }
+
+    // 머리에 노드 삽입
+    public void addFirst(E obj) {
+        Node<E> ptr = head;
+        head = crnt = new Node<E>(obj, ptr);
+    }
+
+    // 꼬리에 노드 삽입
+    public void addList(E obj) {
+        if (head == null)
+            addFirst(obj);
+        else {
+            Node<E> ptr = head;
+
+            // while문이 끝났을 때 ptr은 꼬리 노드를 가리킨다.
+            while (ptr.next != null)
+                ptr = ptr.next;
+            ptr.next = crnt = new Node<E>(obj, null);
+        }
+    }
+
+    // 머리 노드를 삭제
+    public void removeFrist() {
+        if (head != null) {
+            head = crnt = head.next;
+        }
+    }
+
+    // 꼬리 노드를 삭제
+    public void removeList() {
+        if (head != null) {
+            if (head.next == null) // 노드가 하나만 있으면
+                removeFrist(); // 머리 노드를 삭제
+            else {
+                Node<E> ptr = head; // 스캔 중인 노드
+                Node<E> pre = head; // 스캔 중인 노드의 앞쪽 노드
+
+                while (ptr.next != null) {
+                    pre = ptr;
+                    ptr = ptr.next;
+                }
+                pre.next = null;
+                crnt = pre;
+            }
+        }
+    }
+
+    
 }
