56564dcc7b24aa66b15e742ea1ee2528a0fc4bee
김지웅 <godjw96@gmail.com>
2021-11-01 16:20:08 +0900

    Update LinkedList.java

--- src/chap09/LinkedList.java
+++ src/chap09/LinkedList.java
@@ -58,17 +58,17 @@ public class LinkedList<E> {
     }
 
     // 머리 노드를 삭제
-    public void removeFrist() {
+    public void removeFirst() {
         if (head != null) {
             head = crnt = head.next;
         }
     }
 
     // 꼬리 노드를 삭제
-    public void removeList() {
+    public void removeLast() {
         if (head != null) {
             if (head.next == null) // 노드가 하나만 있으면
-                removeFrist(); // 머리 노드를 삭제
+                removeFirst(); // 머리 노드를 삭제
             else {
                 Node<E> ptr = head; // 스캔 중인 노드
                 Node<E> pre = head; // 스캔 중인 노드의 앞쪽 노드
@@ -83,5 +83,60 @@ public class LinkedList<E> {
         }
     }
 
-    
+    // 선택한 노드를 삭제하는 remove 메서드
+    public void remove(Node p) {
+        if (head != null) {
+            if (p == head) {
+                removeFirst();
+            } else {
+                Node<E> ptr = head;
+
+                while (ptr.next != null) {
+                    ptr = ptr.next;
+                    if (ptr == null) return; // p가 리스트에 없다.
+                }
+                ptr.next = p.next;
+                crnt = ptr;
+            }
+        }
+    }
+
+    // 선택한 노드를 삭제
+    public void removeCurrentNode() {
+        remove(crnt);
+    }
+
+    // 모든 노드를 삭제
+    public void clear() {
+        while (head != null) {
+            removeFirst();
+        }
+        crnt = null;
+    }
+
+    // 선택한 노드를 하나 뒤쪽으로 이동
+    public boolean next() {
+        if (crnt == null || crnt.next == null)
+            return false;
+        crnt = crnt.next;
+        return true;
+    }
+
+    // 선택한 노드를 출력
+    public void printCurrentNode() {
+        if (crnt == null)
+            System.out.println("선택한 노드가 없습니다.");
+        else
+            System.out.println(crnt.data);
+    }
+
+    // 모든 노드를 출력
+    public void dump() {
+        Node<E> ptr = head;
+
+        while (ptr != null) {
+            System.out.println(ptr.data);
+            ptr = ptr.next;
+        }
+    }
 }
